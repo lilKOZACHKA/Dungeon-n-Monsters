be67151bb5e02ecc70b5a3cd42dc86ff9993394d
lilKOZACHKA <sidorov1ch221@gmail.com>
2024-04-19 03:20:35 +0300

    Revert "Squashed commit of the following:"
    This reverts commit 619e259687fff76023e569bfb258d4c909097978.

--- Dungeon&Monsters/Assets/Script/Camera/TestCamera.cs
+++ Dungeon&Monsters/Assets/Script/Camera/TestCamera.cs
@@ -2,24 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Scripts.UnitLogic
+public class TestCamera : MonoBehaviour
 {
-    public class TestCamera : MonoBehaviour
-    {
-        public Transform target;
-        public Vector3 offset = new Vector3(0, 2, -10);
-        public float smoothTime = 0.25f;
+    public Transform target;
+public Vector3 offset = new Vector3(0, 2, -10);
+public float smoothTime = 0.25f;
 
-        Vector3 currentVelocity;
+Vector3 currentVelocity;
 
-        private void LateUpdate()
-        {
-            transform.position = Vector3.SmoothDamp(
-                transform.position,
-                target.position + offset,
-                ref currentVelocity,
-                smoothTime
-                );
-        }
-    }
+private void LateUpdate()
+{
+    transform.position = Vector3.SmoothDamp(
+        transform.position, 
+        target.position + offset, 
+        ref currentVelocity, 
+        smoothTime
+        );
+}
 }
